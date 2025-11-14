1ff8855dfb35f09c13e71b2730b1df82cdfd76fc
programacion.lulaculinquieto <91832154+marialuzopazo@users.noreply.github.com>
2025-11-13 23:40:25 -0300

    Modify Vite config for build and server settings
    Updated Vite configuration to change base path and added build options.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/CineStream-TP-Grupal-Prog3/",
-});
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+        // Agrega otras páginas HTML si tienes
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+})
